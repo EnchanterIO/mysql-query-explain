5af40f8331962615c82411bbebab16e5345ee23a
Jose Navarrete <janavarretecristino@gmail.com>
2017-03-30 14:35:03 +0200

    autobahn first attempt.

--- src/Client/app/main.js
+++ src/Client/app/main.js
@@ -1,13 +1,44 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import autobahn from 'autobahn';
 
-var Content = React.createClass({
-  render: function() {
-    return (
-      <article>
-        <p>Hey welcome to MySQL query explain project :)</p>
-      </article>
-    );
-  }
+// const React = require('react');
+// const ReactDOM = require('react-dom');
+
+const Content = React.createClass({
+
+    getInitialState() {
+        return {
+            connection: null
+        }
+    },
+
+    componentWillMount() {
+        this.connection = new autobahn.Connection({
+            url: 'ws://localhost:1337/app',
+            realm: 'realm1'
+        });
+        console.log('hey');
+        this.connection.onopen = function (session) {
+            console.log(session);
+            console.log()
+        }
+        // this.connection = new WebSocket('wss://echo.websocket.org');
+        // this.connection.onmessage = event => {
+        //     console.log(event.data);
+        // }
+        //
+        // setInterval(_ => {
+        //     this.connection.send(Math.random());
+        // }, 10000);
+    },
+
+    render() {
+        return (
+            <article>
+                <header>Hey {this.props.test} welcome to MySQL query explain project :)</header>
+            </article>
+        );
+    }
 });
 ReactDOM.render(<Content />, document.getElementById('content'));
