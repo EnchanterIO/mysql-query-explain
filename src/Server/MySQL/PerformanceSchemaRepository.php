fd4ccda6b742ce776f4bf2cf2be26c0e53662dcf
Lukas Lukac <services@trki.sk>
2017-03-31 11:25:18 +0200

    Adds enable and reset stats option to PerformanceRepository.

--- src/Server/MySQL/PerformanceSchemaRepository.php
+++ src/Server/MySQL/PerformanceSchemaRepository.php
@@ -30,14 +30,23 @@ class PerformanceSchemaRepository
         return $result[0]['Value'] === 'ON';
     }
 
+    /**
+     * @return void
+     */
     public function enableStats()
     {
-
+        $this->connection->execute("UPDATE performance_schema.setup_consumers SET enabled = 'YES' WHERE NAME = 'events_statements_history';");
+        $this->connection->execute("UPDATE performance_schema.setup_consumers SET enabled = 'YES' WHERE NAME LIKE 'events_statements_%';");
+        $this->connection->execute("UPDATE performance_schema.setup_consumers SET enabled = 'YES' WHERE NAME LIKE 'events_stages_%';");
+        $this->connection->execute("UPDATE performance_schema.setup_instruments SET ENABLED = 'YES', TIMED = 'YES' WHERE NAME LIKE 'stage/sql/%';");
     }
 
+    /**
+     * @return void
+     */
     public function resetStats()
     {
-
+        $this->connection->execute("CALL sys.ps_truncate_all_tables(FALSE);");
     }
 
     public function disableStats()
